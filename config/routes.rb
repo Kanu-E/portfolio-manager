315e1181fb55d788d720549950d3e4b28c1f7b7e
kanu-e <chukwuemekakanu@gmail.com>
2020-10-26 14:58:01 -0500

    add bcrypt and password digest

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get 'user/index'
-  get 'user/show'
+ 
+
   get 'user/new'
-  get 'user/edit'
+  post 'user/create'
+
   resources :users
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
-  
+
+   
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
