ec62586e818761710fe25cebeb0cea7570dbad59
kanu-e <chukwuemekakanu@gmail.com>
2020-10-27 18:04:45 -0500

    refactor portfolio show index

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,11 @@
 class Portfolio < ApplicationRecord
     belongs_to :user
-    
+
     def to_s
         "#{first_name} #{last_name}"
     end 
+
+    def portfolio_manager
+        self.user
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :rides
+    has_many :portfolios
 end
