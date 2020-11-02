8c58b51a2c6cdbca04aa7ff43c944b04b80aa8d7
kanu-e <chukwuemekakanu@gmail.com>
2020-11-02 02:08:11 -0600

    create nested routes finance holding

--- app/controllers/holdings_controller.rb
+++ app/controllers/holdings_controller.rb
@@ -1,17 +1,37 @@
 class HoldingsController < ApplicationController
+  before_action :set_holding, only: [:show, :edit, :update]
+  before_action :require_login
 
   def show
   end
 
   def new
     @holding = Holding.new(finance_id: params[:finance_id])
+    if params[:finance_id]
     @finance = Finance.find(params[:finance_id])
+    end
   end
 
   def edit
   end
 
   def create
-    raise params.inspect
+    @holding = Holding.new(holding_params)
+    if @holding.save
+    redirect_to @holding, notice: 'Holding was successfully created.' 
+    else
+    render :new 
+    end
   end
+
+  private
+  
+  def set_holding
+    @holding = Holding.find(params[:id])
+  end
+
+  def holding_params
+    params.require(:holding).permit(:name, :finance_id, :portfolio_id)
+  end
+
 end
